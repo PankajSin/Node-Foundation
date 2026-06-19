77311db2e2ac6a50cefdb79bb5bf48900374e39a
Pankaj Singh <pankaj1523110076@gmail.com>
2026-06-19 14:04:12 +0530

    refactor: optimize file writing using writable streams

--- 16-Node_Performance analysis/speed_test.ts
+++ 16-Node_Performance analysis/speed_test.ts
@@ -1,3 +1,6 @@
+// performance wise analysis -> in line5 we use await inside for loop
+//  Node.js is forced to wait for the hard drive to finish writing before moving to the next number,
+// This makes the code safe on memory, but very slow.n
 // // import fs from 'node:fs/promises';
 
 // (async () => {
@@ -10,15 +13,32 @@
 // })();
 // // through callback
 
-import fs from 'fs';
+// import fs from 'fs';
 
-(() => {
+// (() => {
+//   console.time('timer');
+//   fs.open('test.txt', 'w', (err, fd) => {
+//     // request os to open test.txt and create 'test.txt' in write-only mode so that we can modify its contents.
+//     for (let i = 0; i < 500000; i++) {
+//       fs.write(fd, `${i} `, null, (err) => {
+//         if (err) throw err;
+//       });
+//     }
+//   });
+
+//   console.timeEnd('timer');
+// })();
+
+import fs from 'node:fs/promises';
+
+(async () => {
   console.time('timer');
-  fs.open('test.txt', 'w', (err, fd) => {
-    for (let i = 0; i < 500000; i++) {
-      fs.write(fd);
-    }
-  });
-
+  const fileHandle = await fs.open('test.txt', 'w');
+  const stream = fileHandle.createWriteStream();
+  for (let i = 0; i < 5000000; i++) {
+    const buffer = Buffer.from(` ${i} `);
+    stream.write(buffer);
+  }
+  //  create internal buffer from ram,pack data into internal buffer ,when packed ,make system call to flush entire block onto harddrive
   console.timeEnd('timer');
 })();
